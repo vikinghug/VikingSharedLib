7a5d23ef98f990dd3c7e00aa61c82b715490af38
Kevin Altman <kevin@itsthatguy.com>
2020-03-10 13:32:16 -0400

    Adds name and color management for print function

--- VikingSharedLib.lua
+++ VikingSharedLib.lua
@@ -24,15 +24,37 @@ function lib:EndsWith(str, ending)
   return ending == "" or str:sub(-#ending) == ending
 end
 
+local PrintColors = {
+  addon.Colors.RED:ToText(),
+  addon.Colors.DARK_RED:ToText(),
+  addon.Colors.ORANGE:ToText(),
+  addon.Colors.BLUE:ToText(),
+  addon.Colors.PALE_BLUE:ToText(),
+  addon.Colors.GREEN:ToText(),
+  addon.Colors.DARK_GREEN:ToText(),
+  addon.Colors.PALE_GREEN:ToText(),
+  addon.Colors.YELLOW:ToText(),
+  addon.Colors.PALE_YELLOW:ToText(),
+  addon.Colors.PINK:ToText(),
+  addon.Colors.DARK_PINK:ToText(),
+  addon.Colors.CYAN:ToText(),
+}
+
 function lib:Print(...)
-  local color = lib.Colors:NewRGBA(lib.Colors.YELLOW, 1):ToText()
-  print(color .. addonName .. ":|r", ...)
+  local n = 0
+  (self.name or addonName):gsub(".", function(c) n = n + string.byte(c) end)
+  local color = PrintColors[((n - 1) % #PrintColors) + 1]
+  print(color .. (self.name or addonName) .. [[:|r]], ...)
 end
 
 function lib:Debug(strName, tData)
   if ViragDevTool_AddData then
     ViragDevTool_AddData(tData, strName)
   end
+end
+
+function lib:RegisterName(name)
+  self.name = name
 end
 
 lib.Colors = addon.Colors
