4ac54dc2bc170465d4b845e0bc27a785ca6832a2
Kevin Altman <kevin@itsthatguy.com>
2020-03-15 12:45:20 -0400

    removed grizzly bear 03

--- assets/sounds/sounds.lua
+++ assets/sounds/sounds.lua
@@ -15,77 +15,73 @@ local SOUNDS = {
     path = [[Interface\Addons\]] .. folder .. [[baby-groot.mp3]],
   },
   Bear1 = {
-    name = "bear 1",
+    name = "Bear 01",
     path = [[Interface\Addons\]] .. folder .. [[bear-1.mp3]],
   },
   Bear2 = {
-    name = "bear 2",
+    name = "Bear 02",
     path = [[Interface\Addons\]] .. folder .. [[bear-2.mp3]],
   },
   Bear3 = {
-    name = "bear 3",
+    name = "Bear 03",
     path = [[Interface\Addons\]] .. folder .. [[bear-3.mp3]],
   },
   Bear4 = {
-    name = "bear 4",
+    name = "Bear 04",
     path = [[Interface\Addons\]] .. folder .. [[bear-4.mp3]],
   },
   Bear5 = {
-    name = "bear 5",
+    name = "Bear 05",
     path = [[Interface\Addons\]] .. folder .. [[bear-5.mp3]],
   },
   Bear6 = {
-    name = "bear 6",
+    name = "Bear 06",
     path = [[Interface\Addons\]] .. folder .. [[bear-6.mp3]],
   },
   Bear7 = {
-    name = "bear 7",
+    name = "Bear 07",
     path = [[Interface\Addons\]] .. folder .. [[bear-7.mp3]],
   },
   Bear8 = {
-    name = "bear 8",
+    name = "Bear 08",
     path = [[Interface\Addons\]] .. folder .. [[bear-8.mp3]],
   },
   Bear9 = {
-    name = "bear 9",
+    name = "Bear 09",
     path = [[Interface\Addons\]] .. folder .. [[bear-9.mp3]],
   },
   Bear10 = {
-    name = "bear 10",
+    name = "Bear 10",
     path = [[Interface\Addons\]] .. folder .. [[bear-10.mp3]],
   },
   GrizzlyBear1 = {
-    name = "Grizzly Bear 1",
+    name = "Grizzly Bear 01",
     path = [[Interface\Addons\]] .. folder .. [[grizzly-bear-1.mp3]],
   },
   GrizzlyBear2 = {
-    name = "Grizzly Bear 2",
+    name = "Grizzly Bear 02",
     path = [[Interface\Addons\]] .. folder .. [[grizzly-bear-2.mp3]],
   },
-  GrizzlyBear3 = {
-    name = "Grizzly Bear 3",
-    path = [[Interface\Addons\]] .. folder .. [[grizzly-bear-3.mp3]],
-  },
   Koala = {
-    name = "Koala 1",
+    name = "Koala 01",
     path = [[Interface\Addons\]] .. folder .. [[koala.mp3]],
   },
   PolarBear1 = {
-    name = "Polar Bear 1",
+    name = "Polar Bear 01",
     path = [[Interface\Addons\]] .. folder .. [[polar-bear-1.mp3]],
   },
   PolarBear2 = {
-    name = "Polar Bear 2",
+    name = "Polar Bear 02",
     path = [[Interface\Addons\]] .. folder .. [[polar-bear-2.mp3]],
   },
   PolarBear3 = {
-    name = "Polar Bear 3",
+    name = "Polar Bear 03",
     path = [[Interface\Addons\]] .. folder .. [[polar-bear-3.mp3]],
   },
 }
 
 for key, data in pairs(SOUNDS) do
-  LSM:Register("sound", data.name, data.path)
+  LSM:Register("sound", "Viking: " .. data.name, data.path)
 end
 
 addon.SOUNDS = SOUNDS
